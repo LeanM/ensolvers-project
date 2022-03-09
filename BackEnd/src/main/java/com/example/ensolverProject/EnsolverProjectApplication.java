05a8c83f3b6b49e590359abbc71ddb50e0ae05af
LeanM <leandro.moran.11@gmail.com>
2022-03-09 09:00:53 -0300

    Back End Spring Boot
    Create the Spring boot app

---
+++ BackEnd/src/main/java/com/example/ensolverProject/EnsolverProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ensolverProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsolverProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsolverProjectApplication.class, args);
+	}
+
+}
